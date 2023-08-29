0b7c5e93c25062cf31a38b8a1ece1ab3d19c403b
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2023-08-29 08:05:20 +0800

    Split numerical integrator from trajectory calculator

---
+++ ballistics/integration.py
@@ -0,0 +1,129 @@
+from abc import ABC, abstractmethod
+from typing import Callable
+
+import numpy as np
+
+
+class NumericalIntegrator(ABC):
+    @abstractmethod
+    def __init__(
+        self,
+        x: np.ndarray,
+        v: np.ndarray,
+        accel_func: Callable[[np.ndarray], np.ndarray]
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def step(
+        self,
+        accel_func: Callable[[np.ndarray], np.ndarray],
+        dt: float
+    ) -> (np.ndarray, np.ndarray):
+        pass
+
+
+class EulerMethodIntegrator(NumericalIntegrator):
+    def __init__(
+        self,
+        x: np.ndarray,
+        v: np.ndarray,
+        accel_func: Callable[[np.ndarray], np.ndarray]
+    ) -> None:
+        self.x = x.copy()
+        self.v = v.copy()
+
+    def step(
+        self,
+        accel_func: Callable[[np.ndarray], np.ndarray],
+        dt: float
+    ) -> (np.ndarray, np.ndarray):
+        self.x += self.v * dt
+        self.v += accel_func(self.v) * dt
+        return self.x, self.v
+
+
+class HeunsMethodIntegrator(NumericalIntegrator):
+    def __init__(
+        self,
+        x: np.ndarray,
+        v: np.ndarray,
+        accel_func: Callable[[np.ndarray], np.ndarray]
+    ) -> None:
+        self.x = x.copy()
+        self.v = v.copy()
+
+    def step(
+        self,
+        accel_func: Callable[[np.ndarray], np.ndarray],
+        dt: float
+    ) -> (np.ndarray, np.ndarray):
+        accel = accel_func(self.v)
+        v_pred = self.v + accel_func(self.v) * dt
+        accel_pred = accel_func(v_pred)
+
+        self.x += (self.v + v_pred) / 2.0 * dt
+        self.v += (accel + accel_pred) / 2.0 * dt
+
+        return self.x, self.v
+
+
+class BeemansAlgorithmIntegrator(NumericalIntegrator):
+    def __init__(
+        self,
+        x: np.ndarray,
+        v: np.ndarray,
+        accel_func: Callable[[np.ndarray], np.ndarray]
+    ) -> None:
+        self.x = x.copy()
+        self.v = v.copy()
+        self.accel_prev = accel_func(self.v)
+
+    def step(
+        self,
+        accel_func: Callable[[np.ndarray], np.ndarray],
+        dt: float
+    ) -> (np.ndarray, np.ndarray):
+        accel_curr = accel_func(self.v)
+        v_next = self.v + (3.0 * accel_curr - self.accel_prev) / 2.0 * dt
+        accel_next = accel_func(v_next)
+
+        x_next = self.x + self.v * dt + \
+            (accel_next + 2.0 * accel_curr) / 6.0 * dt * dt
+        v_next = self.v + (5.0 * accel_next + 8.0 *
+                           accel_curr - self.accel_prev) / 12.0 * dt
+        accel_next = accel_func(v_next)
+
+        self.x = x_next
+        self.v = v_next
+        self.accel_prev = accel_next
+
+        return self.x, self.v
+
+class RungeKuttaMethodIntegrator(NumericalIntegrator):
+    def __init__(
+        self,
+        x: np.ndarray,
+        v: np.ndarray,
+        accel_func: Callable[[np.ndarray], np.ndarray]
+    ) -> None:
+        self.x = x.copy()
+        self.v = v.copy()
+
+    def step(
+        self,
+        accel_func: Callable[[np.ndarray], np.ndarray],
+        dt: float
+    ) -> (np.ndarray, np.ndarray):
+        k1 = accel_func(self.v)
+        v_k1 = self.v + k1 * dt / 2.0
+        k2 = accel_func(v_k1)
+        v_k2 = self.v + k2 * dt / 2.0
+        k3 = accel_func(v_k2)
+        v_k3 = self.v + k3 * dt
+        k4 = accel_func(v_k3)
+
+        self.x += (self.v + 2.0 * v_k1 + 2.0 * v_k2 + v_k3) / 6.0 * dt
+        self.v += (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0 * dt
+        
+        return self.x, self.v
--- ballistics/trajectory.py
+++ ballistics/trajectory.py
@@ -1,92 +1,11 @@
-from . import environment
+from environment import *
+from integration import *
+
 import numpy as np
-from numpy import linalg
 from scipy.interpolate import make_interp_spline
 
 
-def numerical_solve(v0, bc, time_of_flight, cd_func):
-    dt = 1 / 60
-
-    temp = 59
-    pressure = 29.92
-    rh = 50
-    wv_pressure = 0.50
-
-    v_sound = 49.0223*(temp + 459.67)**0.5
-
-    density_air_std = 0.0764742
-    density_air = (pressure / 29.92) * (518.67 /
-                                        (temp + 459.67)) * density_air_std
-
-    v_sound_corr = 1 + 0.0014 * rh * wv_pressure / 29.92
-    density_air_corr = 1 - 0.00378 * rh * wv_pressure / 29.92
-
-    v_sound *= v_sound_corr
-    density_air *= density_air_corr
-
-    def cd_star(speed):
-        m = speed / v_sound
-        result = density_air * np.pi * cd_func(m) / (1152.0 * bc)
-        return result
-
-    ts = []
-    xs = []
-    vs = []
-
-    t = 0.0
-    x = np.array([0.0, 0.0, 0.0])
-    v = v0.copy()
-    g = np.array([0.0, 0.0, -32.17405])
-
-    speed = linalg.norm(v)
-    accel = -cd_star(speed) * speed * v + g
-
-    while t < time_of_flight:
-        # Runge-Kutta 4th order
-        # x += v * dt
-        # speed = linalg.norm(v)
-        # k1 = -cd_star(speed) * speed * v + g
-        # y = v + k1/2 * dt
-        # speed = linalg.norm(y)
-        # k2 = -cd_star(speed) * speed * y + g
-        # y = v + k2/2 * dt
-        # speed = linalg.norm(y)
-        # k3 = -cd_star(speed) * speed * y + g
-        # y = v + k3 * dt
-        # speed = linalg.norm(y)
-        # k4 = -cd_star(speed) * speed * y + g
-        # v += (k1 + 2*k2 + 2*k3 + k4) * dt / 6
-        # t += dt
-
-        # Euler
-        # x += v * dt
-        # speed = linalg.norm(v)
-        # accel = -cd_star(speed) * speed * v + g
-        # v += accel * dt
-        # t += dt
-
-        # Heun
-        # speed = linalg.norm(v)
-        # accel = -cd_star(speed) * speed * v + g
-        # v_next = v + accel * dt
-        # x += (v + v_next) * dt / 2
-        # v = v_next
-        # t += dt
-
-        # Verlet
-        x = x + v * dt + accel * 0.5 * dt * dt
-        speed = linalg.norm(v)
-        new_accel = -cd_star(speed) * speed * v + g
-        v = v + (accel + new_accel) * 0.5 * dt
-        accel = new_accel
-        t += dt
-
-        ts.append(t)
-        xs.append(x)
-        vs.append(v)
-
-    return ts, xs, vs
-
+MAX_SIMULATION_TIME = 20.0
 
 def parse_drag_table(filename: str):
     table = []
@@ -96,23 +15,181 @@ def parse_drag_table(filename: str):
     return table
 
 
+class PointMassTrajectory:
+    
+    def __init__(self, integrator: type[NumericalIntegrator]) -> None:
+        g1 = parse_drag_table('data/mcg1.txt')
+        g7 = parse_drag_table('data/mcg7.txt')
+
+        self.g1_curve = make_interp_spline(*zip(*g1), k=3)
+        self.g7_curve = make_interp_spline(*zip(*g7), k=3)
+        self.cd_func = self.g7_curve
+
+        self.integrator = integrator
+
+        self.g = np.array([0.0, 0.0, -32.17405])
+
+    def calculate_acceleration(
+        self,
+        v: np.ndarray,
+        v_sound: float,
+        bc: float,
+        density_air: float,
+        wind: np.ndarray
+    ) -> np.ndarray:
+
+        # 8 * 144, the 144 comes from converting in2 to ft2
+        k = 1152.0
+
+        vw = v - wind
+        speed = np.linalg.norm(vw)
+        m = speed / v_sound
+        cd_star = density_air * np.pi * self.cd_func(m) / (k * bc)
+        decel = -cd_star * speed * vw + self.g
+        return decel
+
+    def firing_angle_at_zero(
+        self,
+        muzzle_speed: float,
+        bc: float,
+        sight_height: float,
+        barrel_length: float,
+        distance_of_zero: float,
+        elevation_of_zero: float,
+        dt: float,
+        wind: np.ndarray = np.zeros(3),
+        temp: float = 59.0,
+        pressure: float = 29.92,
+        rh: float = 0.0,
+    ):
+        
+        NUM_CONVERGENCE_STEPS = 100
+
+        density_air = air_density(temp, pressure, rh, 0.0)
+        v_sound = speed_sound(temp, rh, 0.0)
+
+        def accel_func(v):
+            return self.calculate_acceleration(v, v_sound, bc, density_air, wind)
+        
+        # Where the bullet trajectory starts
+        x0 = np.array([barrel_length, 0.0, -sight_height])
+
+        # Initial guess of vertical angle
+        angle = np.arctan(sight_height / distance_of_zero)
+
+        vert_angle_low = angle - np.radians(45)
+        vert_angle_high = angle + np.radians(45)
+
+        # Solve for vertical angle
+        for _ in range(NUM_CONVERGENCE_STEPS):
+            angle = (vert_angle_low + vert_angle_high) / 2.0
+            
+            v0 = np.array([muzzle_speed * np.cos(angle),
+                          0.0, muzzle_speed * np.sin(angle)])
+
+            integrator = self.integrator(x0, v0, accel_func)
+
+            x_prev = x0.copy()
+
+            for _ in np.arange(0.0, MAX_SIMULATION_TIME, dt):
+                x, _ = integrator.step(accel_func, dt)
+                if x[0] > distance_of_zero:
+
+                    drop = np.interp(
+                        distance_of_zero,
+                        [x_prev[0], x[0]],
+                        [x_prev[2], x[2]]
+                    )
+
+                    if drop > elevation_of_zero:
+                        # Aiming too high
+                        vert_angle_high = angle
+                    else:
+                        # Aiming too low
+                        vert_angle_low = angle
+                    break
+                x_prev = x.copy()
+            else:
+                # Did not reach the zero target, either angle too high or bullet is not fast enough
+                vert_angle_high = angle
+
+        return angle
+
+    def calculate_trajectory(
+        self,
+        x0: np.ndarray,
+        v0: np.ndarray,
+        bc: float,
+        max_range: float,
+        dt: float,
+        wind: np.ndarray = np.zeros(3),
+        temp: float = 59.0,
+        pressure: float = 29.92,
+        rh: float = 0.0,
+    ) -> (list[float], list[np.ndarray], list[np.ndarray]):
+        
+        density_air = air_density(temp, pressure, rh, 0.0)
+        v_sound = speed_sound(temp, rh, 0.0)
+
+        def accel_func(v):
+            return self.calculate_acceleration(v, v_sound, bc, density_air, wind)
+        
+        ts = [0.0]
+        xs = [x0.copy()]
+        vs = [v0.copy()]
+        
+        integrator = self.integrator(x0, v0, accel_func)
+        for t in np.arange(dt, MAX_SIMULATION_TIME, dt):
+            x, v = integrator.step(accel_func, dt)
+
+            ts.append(t)
+            xs.append(x.copy())
+            vs.append(v.copy())
+
+            if x[0] > max_range:
+                break
+
+        return ts, xs, vs
+
+
 def main():
-    g7 = parse_drag_table('data/mcg7.txt')
-    cd_func = make_interp_spline(*zip(*g7), k=3)
-
-    bc = 0.371
+    pm_traj = PointMassTrajectory(RungeKuttaMethodIntegrator)
     muzzle_speed = 2970
-    v0 = np.array([muzzle_speed, 0.0, 0.0])
-    time_of_flight = 4.0
+    bc = 0.371
+    sight_height = 1.5 / 12.0
+    barrel_length = 0.0
+    distance_of_zero = 100.0 * 3.0
+    elevation_of_zero = 0.0
+    max_range = 2500.0 * 3.0
+    dt = 1/60
+    
+    angle = pm_traj.firing_angle_at_zero(
+        muzzle_speed,
+        bc,
+        sight_height,
+        barrel_length,
+        distance_of_zero,
+        elevation_of_zero,
+        dt,
+        rh = 50
+    )
 
-    ts, xs, vs = numerical_solve(v0, bc, time_of_flight, cd_func)
+
+    x0 = np.array([barrel_length, 0.0, -sight_height])
+    v0 = np.array([muzzle_speed * np.cos(angle), 0.0, muzzle_speed * np.sin(angle)])
+
+    # def sight_line(x):
+    #     return np.interp(x, [0.0, 0.0], [0.0, elevation_of_zero])
+    
+    ts, xs, vs = pm_traj.calculate_trajectory(x0, v0, bc, max_range, dt, rh=50)
+
     rs = []
     ds = []
     ss = []
     for x, v in zip(xs, vs):
         rs.append(x[0])
         ds.append(x[2])
-        ss.append(linalg.norm(v))
+        ss.append(np.linalg.norm(v))
 
     drop_curve = make_interp_spline(rs, ds, k=3)
     speed_curve = make_interp_spline(rs, ss, k=3)
@@ -146,39 +223,7 @@ def main():
             )
         )
 
-    bc = float('inf')
-    muzzle_speed = 262.467
-    angle = 2005.03 / 60
-    v0 = np.array([muzzle_speed * np.cos(angle * np.pi / 180),
-                  0.0, muzzle_speed * np.sin(angle * np.pi / 180)])
-    time_of_flight = 10.0
-
-    ts, xs, vs = numerical_solve(v0, bc, time_of_flight, cd_func)
-    rs = []
-    ds = []
-    ss = []
-    for x, v in zip(xs, vs):
-        rs.append(x[0])
-        ds.append(x[2])
-        ss.append(linalg.norm(v))
-
-    drop_curve = make_interp_spline(rs, ds, k=3)
-    speed_curve = make_interp_spline(rs, ss, k=3)
-    time_curve = make_interp_spline(rs, ts, k=3)
-
-    print('')
-    print('Vacuum trajectory')
-    for x in range(0, 650, 50):
-        x /= 0.3048
-        print(
-            '%4d, %8.2f, %8.3f, %6.3f' %
-            (
-                x * 0.3048,
-                12 * drop_curve(x),
-                speed_curve(x),
-                time_curve(x)
-            )
-        )
+    
 
 
 if __name__ == '__main__':
